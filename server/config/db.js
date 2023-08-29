77bbc88963f4103164f2a39c08b3f724926f057d
nonshalant <shamir.gibbons@gmail.com>
2023-08-29 13:07:08 -0400

    updating

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${db}?ssl=true`, {
+    await mongoose.connect(db, {
       useNewUrlParser: true
     });
     console.log('Mongo connected!!!');
