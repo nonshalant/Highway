1e38eeee88c465616c25ee3da43aab985c80c7ea
nonshalant <shamir.gibbons@gmail.com>
2023-08-30 10:18:47 -0400

    connected to mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,7 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true
-    });
+    await mongoose.connect(db, { ssl: true });
     console.log('Mongo connected!!!');
   } catch (error) {
     console.error(error.message);
